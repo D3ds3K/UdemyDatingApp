99bca28dec47c2de6165787a2fd8571bf4486fd0
D3ds3K <32746977+D3ds3K@users.noreply.github.com>
2024-05-23 00:47:42 +0530

    Dating app Section 2 commit Udemy

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,32 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController : ControllerBase
+{
+    private readonly AppUserContext _appUserContext;
+
+    public UsersController(AppUserContext appUserContext)
+    {
+        _appUserContext = appUserContext;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
+        var users = await _appUserContext.Users.ToListAsync();
+        return users;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<AppUser>> GetUser(int id)
+    {
+        var user = await _appUserContext.Users.FindAsync(id);
+        return user;
+    }
+}
---
+++ API/Data/AppUserContext.cs
@@ -0,0 +1,12 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class AppUserContext : DbContext
+{
+    public AppUserContext(DbContextOptions options) : base(options)
+    {
+    }
+    public DbSet<AppUser> Users { get; set; }
+}
